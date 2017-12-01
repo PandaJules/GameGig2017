ec21c87526b6e97f8d005569b9b47b8f7d669759
Julia <yb259@cam.ac.uk>
2017-12-01 11:41:46 +0000

    basic player moving up/down/left/right while changing the image

--- game/main.lua
+++ game/main.lua
@@ -12,58 +12,66 @@ function love.load()
 
 	coins = {}
 	
-	score = 0
+	lives = 5
 
 	sounds = {}
 	sounds.coin = love.audio.newSource("assets/sounds/coin.ogg", "static")
 
 	fonts = {}
-	fonts.large = love.graphics.newFont("assets/fonts/Gamer.ttf", 50)
+	fonts.large = love.graphics.newFont("assets/fonts/Gamer.ttf", 80)
 
 
 	images = {}
 	images.background = love.graphics.newImage("assets/images/ground.png")
-	images.coin = love.graphics.newImage("assets/images/coin.png")
+	--images.coin = love.graphics.newImage("assets/images/coin.png")
 	images.player_down = love.graphics.newImage("assets/images/player_down.png")
-	images.left = love.graphics.newImage("assets/images/player_left.png")
+	images.player_right = love.graphics.newImage("assets/images/player_right.png")
+	images.player_left = love.graphics.newImage("assets/images/player_left.png")
+	images.player_up = love.graphics.newImage("assets/images/player_up.png")
+
 
 end
 
 SPEED = 500
 
 function love.update(dt)
+	move(dt)
+end
+
+function move(dt)
 	if love.keyboard.isDown("right") then 
 		player.x = player.x + 10
 		player.direction = "right"
 	elseif love.keyboard.isDown("left") then
 		player.x = player.x - 10
+		player.direction = "left"
 	elseif love.keyboard.isDown("up") then
 		player.y = player.y - dt * SPEED
+		player.direction = "up"
 	elseif love.keyboard.isDown("down") then
 		player.y = player.y + dt * SPEED
 		player.direction = "down"
 	end 
 
 
-	for i=#coins, 1, -1 do
-		local coin = coins[i]
-		if AABB(player.x, player.y, player.w, player.h, coin.x, coin.y, coin.r) then
-			table.remove(coins, i)
-			score = score +1 
-			sounds.coin:play()
-		end
-	end
+	-- for i=#coins, 1, -1 do
+	-- 	local coin = coins[i]
+	-- 	if AABB(player.x, player.y, player.w, player.h, coin.x, coin.y, coin.r) then
+	-- 		table.remove(coins, i)
+	-- 		score = score +1 
+	-- 		sounds.coin:play()
+	-- 	end
+	-- end
 
 
-	if math.random()<0.02 then
-		local coin
-		coin = {}
-		coin.r = 25
-		coin.x = math.random(0, 800-2 * coin.r)
-		coin.y = math.random(0, 600-2 * coin.r)
-		table.insert(coins, coin)
-	end	
-
+	-- if math.random()<0.02 then
+	-- 	local coin
+	-- 	coin = {}
+	-- 	coin.r = 25
+	-- 	coin.x = math.random(0, 800-2 * coin.r)
+	-- 	coin.y = math.random(0, 600-2 * coin.r)
+	-- 	table.insert(coins, coin)
+	-- end	
 end
 
 
@@ -75,13 +83,25 @@ function love.draw()
 	end
 
 	love.graphics.setColor(255, 255, 0)
-	for i=1, #coins, 1 do
-		local coin = coins[i]
-		love.graphics.circle("fill", coin.x, coin.y, coin.r)
+	-- for i=1, #coins, 1 do
+	-- 	local coin = coins[i]
+	-- 	love.graphics.circle("fill", coin.x, coin.y, coin.r)
+	-- end
+	local img = images.player_down
+	if player.direction == "right" then
+			img = images.player_right
+	elseif player.direction == "up" then
+			img = images.player_up
+	elseif player.direction == "left" then
+			img = images.player_left
+	elseif player.direction == "down" then
+			img = images.player_down
+	else
+			img = images.player_right
 	end
 
-	love.graphics.draw(images.player_down, player.x, player.y)
+	love.graphics.draw(img, player.x, player.y)
 	love.graphics.setFont(fonts.large)
-	love.graphics.print("SCORE: " .. score, 10, 10) 
+	love.graphics.print("LIVES: " .. lives, 10, 10) 
 
 end
